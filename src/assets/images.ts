53bb297e5722c3fd32eb3e98c00d70b590b98f4d
Youichi-Higa <yo1_1023@yahoo.co.jp>
2022-11-04 21:02:17 +0900

    画像追加、レイアウト修正

---
+++ src/assets/images.ts
@@ -0,0 +1,10 @@
+const imagesDir = `${process.env.PUBLIC_URL}/images`;
+
+export const images = {
+  complete: `${imagesDir}/complete.png`,
+  getWallet: `${imagesDir}/get-wallet.png`,
+  giveWallet: `${imagesDir}/give-wallet.png`,
+  step1: `${imagesDir}/step1.svg`,
+  step2: `${imagesDir}/step2.svg`,
+  step3: `${imagesDir}/step3.svg`,
+};
